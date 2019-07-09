34c42261d583950da97b1e5b51aa1bc14db80b7d
Weilun Liu <763027562@qq.com>
2019-07-09 23:18:57 +0000

    user can create topics

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Topic;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 
@@ -29,12 +31,17 @@ class TopicsController extends Controller
 
 	public function create(Topic $topic)
 	{
-		return view('topics.create_and_edit', compact('topic'));
+	    $categories = Category::all();
+	    return view('topics.create_and_edit', compact('topic', 'categories'));
 	}
 
-	public function store(TopicRequest $request)
+	public function store(TopicRequest $request,Topic $topic)
 	{
-		$topic = Topic::create($request->all());
+	    $topic->fill($request->all());
+	    $topic->user_id = Auth::id();
+	    $topic->save();
+	    
+	    
 		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
 	}
 
