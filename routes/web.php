4e60a8bbdbcee3264307eac711dc1809f0c8a869
Weilun Liu <763027562@qq.com>
2019-07-16 03:17:00 +0000

    finilizing the backend authentication dashboard features

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,8 @@ Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
 
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
 
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
+
 
 
 
